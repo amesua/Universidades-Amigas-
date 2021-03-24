2de7478080518a12a67ca891a88b3798d9b98677
amesua <ames.980519@gmail.com>
2021-03-24 16:57:51 -0500

    Creacion de paquetes

---
+++ CartelAnuncios/src/main/java/poli/edu/co/CartelAnunciosApplication.java
@@ -0,0 +1,13 @@
+package poli.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartelAnunciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartelAnunciosApplication.class, args);
+	}
+
+}
---
+++ CartelAnuncios/src/test/java/poli/edu/co/CartelAnunciosApplicationTests.java
@@ -0,0 +1,13 @@
+package poli.edu.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartelAnunciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
