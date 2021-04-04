3353af18b91f6e0b31922afb815523a3a05ac1b9
amesua <ames.980519@gmail.com>
2021-04-03 20:44:18 -0500

    Controller

--- CartelAnuncios/src/main/java/Controller/cartelController.java
+++ CartelAnuncios/src/main/java/Controller/cartelController.java
@@ -1,11 +1,43 @@
 package Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import Model.Cartel;
 
 @RestController 
 @RequestMapping("/api/v1/")
 public class cartelController {
 
+	@Autowired
+	Repository.cartelRepository cartelRepository;
+	
+	@GetMapping("/Carteles")
+	public List<Cartel> getAllCarteles(){
+		return cartelRepository.findAll();
+	}
+	
+	@GetMapping("/Carteles/{id}")
+	public Cartel getCustomerByID(@PathVariable String id) {
+		return cartelRepository.findById(id).get();
+	}
+	
+	@PostMapping("/Carteles")
+	public Cartel saveCustomer(@RequestBody Cartel cartel){
+		return cartelRepository.save(cartel);
+	}
+	
+	@PostMapping("/CartelesList")
+	public List<Cartel> saveListCustomers(@RequestBody List<Cartel> carteles) {
+		return cartelRepository.saveAll(carteles);
+	}
+	
 	
 }
