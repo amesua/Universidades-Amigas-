e5dc6efbd64a9d695b250d5b398c529da8f09301
amesua <ames.980519@gmail.com>
2021-05-03 09:46:53 -0500

    pa kuro

--- CartelAnuncios/src/main/java/poli/edu/co/Controller/cartelController.java
+++ CartelAnuncios/src/main/java/poli/edu/co/Controller/cartelController.java
@@ -3,6 +3,7 @@ package poli.edu.co.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +39,23 @@ public class cartelController {
 	public List<Cartel> saveListCustomers(@RequestBody List<Cartel> carteles) {
 		return cartelRepository.saveAll(carteles);
 	}
-	
+	@DeleteMapping("/Carteles/{id}")
+	public Cartel deleteCartel(@PathVariable String id) {
+		Cartel cartel = cartelRepository.findById(id).get();
+		cartelRepository.deleteById(id);
+		return cartel;
+	}
+	//Query
+		@GetMapping("/CustomersQ1/{lastname}")
+		public List<Cartel> findCustomers1(@PathVariable String nombreEvento){
+			return cartelRepository.findCartelesQ1(nombreEvento);
+		}
+
+		//Query
+		/*@GetMapping("/CustomersQ2")
+		public List<Cartel> findCustomers2(){
+			return cartelRepository.findCustomersQ2();
+		}*/
 	
 	
 }
--- CartelAnuncios/src/main/java/poli/edu/co/Repository/cartelRepository.java
+++ CartelAnuncios/src/main/java/poli/edu/co/Repository/cartelRepository.java
@@ -1,9 +1,19 @@
 package poli.edu.co.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import poli.edu.co.Model.Cartel;
 
 public interface cartelRepository extends MongoRepository<Cartel, String> {
+	
+	@Query("{ 'nombreEvento' : /.*?0.*/ }")
+	List<Cartel> findCartelesQ1(String nombreEvento);
+	
+	/*@Query(value="{}", fields= "{_id:0, nombreEvento:1, detalleEvento:1}")
+	List<Cartel> findCustomersQ2();
+	*/
 
 }
